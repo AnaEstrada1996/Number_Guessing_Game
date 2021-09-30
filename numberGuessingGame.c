bc2a863abec9b143ec4e6c1c742dd6e7e46f50f1
Ana Estrada <avestrada@cpp.edu>
2021-09-30 14:01:27 -0700

    Created an outline for number guesing game

---
+++ numberGuessingGame.c
@@ -0,0 +1,40 @@
+/*Create an interactive program that allows a user to guess a number between 1-10. 
+You should create a menu that will have options (this should be displayed on the screen):*/
+
+/*Press 1 to play a gamePress 
+2 to change the max numberPress 
+3 to quit*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+
+    //declare variables 
+
+
+    //random numbers
+
+    //loop for the menu
+
+    //if option 1 is chosen 
+    //print to the user to enter a number 
+    //if the input is equal to the random number output you won! 
+
+    //if output is not equal to the random number 
+    //if num is too high, let them know and allow themm to guess again
+    //if num is too low, let them know and allow them to guess again 
+    //if they press q, take them back to the menu
+
+    //if option 2 is chosen 
+    //tell them about the max number they can set it to 
+    //check for negative numbers
+    //checl if the num is too high
+
+    //if option 3 is chosen
+    //thank the user and end the game 
+
+    return 0;
+}
